c5417f9bb86dd7eefc689eaf1eaf726b1407137b
Andrew Ramstad <aramstad@axiomsoftwareinc.com>
2008-07-14 15:53:12 +0000

    [project @ 28] App Shutdown Debug

--- apps/test/Global/onstart.js
+++ apps/test/Global/onstart.js
@@ -1,4 +1,6 @@
 function run_tests(){
 	axiom.SystemFile.writeToFile(axiom.Test.XMLReport(axiom.Test.run(axiom.Test.globalSuite())).toXMLString(), app.getDir()+java.io.File.separator+'test-results.xml');
+	app.log('Begin System Shutdown');
 	java.lang.System.exit(0);
+	app.log('End System Shutdown');
 }
