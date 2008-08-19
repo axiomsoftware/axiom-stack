27f3951096f09f6effb84f26db7b853fe0ea1158
Ali Beyad <ali@axiomsoftwareinc.com>
2008-08-19 13:19:23 -0400

    License on the top of each source file

--- src/java/axiom/objectmodel/dom/EmbeddedDbConvertor.java
+++ src/java/axiom/objectmodel/dom/EmbeddedDbConvertor.java
@@ -1,3 +1,23 @@
+/*
+ * Axiom Stack Web Application Framework
+ * Copyright (C) 2008  Axiom Software Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Axiom Software Inc., 11480 Commerce Park Drive, Third Floor, Reston, VA 20191 USA
+ * email: info@axiomsoftwareinc.com
+ */
 package axiom.objectmodel.dom;
 
 import java.io.File;
