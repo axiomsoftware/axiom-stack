09d72afaf918551f9e130a5aa65b94575e7ec0be
Ali Beyad <ali@axiomsoftwareinc.com>
2008-08-19 13:19:23 -0400

    License on the top of each source file

--- src/java/axiom/framework/core/ActionSecurityManager.java
+++ src/java/axiom/framework/core/ActionSecurityManager.java
@@ -1,13 +1,23 @@
-/**
- * Axiom class for Security Management
- * 
- * This class implements the security model for Axiom
- * Basically, the components in a URL are traversed and the permissions are checked on 
- * each object and each action to ensure they are permitted by the current user of the 
- * system.
- * 
+/*
+ * Axiom Stack Web Application Framework
+ * Copyright (C) 2008  Axiom Software Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Axiom Software Inc., 11480 Commerce Park Drive, Third Floor, Reston, VA 20191 USA
+ * email: info@axiomsoftwareinc.com
  */
-
 package axiom.framework.core;
 
 import java.util.*;
--- src/java/axiom/objectmodel/db/TransSource.java
+++ src/java/axiom/objectmodel/db/TransSource.java
@@ -1,33 +1,35 @@
+/*
+ * Axiom Stack Web Application Framework
+ * Copyright (C) 2008  Axiom Software Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Axiom Software Inc., 11480 Commerce Park Drive, Third Floor, Reston, VA 20191 USA
+ * email: info@axiomsoftwareinc.com
+ */
 package axiom.objectmodel.db;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.Properties;
 
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.SegmentInfo;
-import org.apache.lucene.index.SegmentInfos;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.IndexInput;
-import org.apache.lucene.store.TransFSDirectory;
-
-import axiom.extensions.trans.TransactionException;
 import axiom.framework.ErrorReporter;
 import axiom.framework.core.Application;
-import axiom.objectmodel.DatabaseException;
-import axiom.objectmodel.dom.LuceneManager;
 import axiom.util.ResourceProperties;
 
 public class TransSource {
