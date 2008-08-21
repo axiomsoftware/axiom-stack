d53facab7e0ee53bf532a22505fd8b2811661105
Ali Beyad <ali@axiomsoftwareinc.com>
2008-08-21 11:03:02 -0400

    Licensing

--- src/js/Global/Number.js
+++ src/js/Global/Number.js
@@ -14,6 +14,12 @@
  * $Date: 2006/04/24 07:02:17 $
  */
 
+/* 
+ * Modified by:
+ * 
+ * Axiom Software Inc., 11480 Commerce Park Drive, Third Floor, Reston, VA 20191 USA
+ * email: info@axiomsoftwareinc.com
+ */
 
 /**
  * Return the SMALLINT string representation of this Number.  When doing a search with a
