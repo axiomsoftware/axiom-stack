71f4f833658c0ab6b0e5df9370873b2d3849e620
Thomas Mayfield <tmayfield@axiomsoftwareinc.com>
2008-09-04 17:05:06 -0400

    add stub file for hook documentation

---
+++ src/js-for-doc/hooks.js
@@ -0,0 +1,78 @@
+/**
+ *  @hook
+ */
+function onCommit(/**Array*/ insertedNodes, /**Array*/ updatedNodes, /**Array*/ deletedNodes){
+
+}
+
+/**
+ *  @hook
+ */
+function postCommit(){
+
+}
+
+/**
+ *  @hook
+ */
+function onStop(){
+
+}
+
+/**
+ *  @hook
+ */
+function onLogout(){
+
+}
+
+/**
+ *  @hook
+ */
+function onStart(){
+
+}
+
+/**
+ *  @hook
+ */
+function onSessionTimeout(/**Session*/ session){
+
+}
+
+/**
+ *  @hook
+ */
+AxiomObject.notfound = function() {
+
+};
+
+/**
+ *  @hook
+ */
+AxiomObject.error = function() {
+
+};
+
+/**
+ *  @hook
+ */
+AxiomObject.unauthorized = function() {
+
+};
+
+/**
+ *  @hook
+ */
+AxiomObject.isAllowed = function(action){
+
+}
+
+/**
+ *  @hook
+ */
+AxiomObject.onPersist = function(){
+
+}
+
+// onCodeUpdate ?
