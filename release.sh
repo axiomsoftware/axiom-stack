e57e7a9d3319c43c199b800e1e38a964da14b313
Andrew Ramstad <aramstad@axiomsoftwareinc.com>
2008-09-16 10:27:34 -0400

    Fixes release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ "$1" ]; then
-	sed 's/public static final String version.*$/public static final String version = "'$1'";/' src/java/axiom/main/Server.java > src/java/axiom/main/Server.java.new
-	mv src/java/axiom/main/Server.java.new src/java/axiom/main/Server.java
+	sed -i 's/public static final String version.*$/public static final String version = "'$1'";/' src/java/axiom/main/Server.java
 	bzr commit src/java/axiom/main/Server.java -m "Release $1"
 else
 	echo usage: release.sh version
