8b472ec9fee5a148a3742a85f6e83eb36e60fe29
Nick <nicholas.j.campbell@gmail.com>
2009-08-01 23:24:34 -0400

    Changed code to use a LinkedHashSet for the onstart functions. Also, changed the concat code for module based onstarts. Module onstarts are run before the application onstarts (because the application is loaded last).

--- src/java/axiom/framework/core/Application.java
+++ src/java/axiom/framework/core/Application.java
@@ -402,7 +402,7 @@ public final class Application implements IPathElement, Runnable {
 		// get class name of root object if defined. Otherwise native Axiom objectmodel will be used.
 		rootObjectClass = classMapping.getProperty("root");
 
-		onstartFunctions = new HashSet<String>();
+		onstartFunctions = new LinkedHashSet<String>();
 		updateProperties();
 
 		dbSources = new Hashtable<String, DbSource>();
@@ -2276,6 +2276,7 @@ public final class Application implements IPathElement, Runnable {
 			}
 			
 			String onStart = props.getProperty("onStart");
+                        System.out.println("onstarts: " + onStart);
 			if (onStart != null) {
 				for (String funcs : onStart.split(",")) {
 					this.onstartFunctions.add(funcs.trim());
