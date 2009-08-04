6b94b073a300aa3054b778087250b09dfe5f4f8f
Nick <nicholas.j.campbell@gmail.com>
2009-08-03 22:35:15 -0400

    TALE errors with show of inner errors.

--- src/js/Global/tal.js
+++ src/js/Global/tal.js
@@ -90,13 +90,15 @@ TAL.Error = function(error, data, expression) {
 		<style type="text/css">
 		    /*<![CDATA[*/
 		    body{font-family:sans-serif}
+		    h1{padding:0 0 0 5px;margin:0 0 10px 0;background:#669933;color:#fff;}
+		    h3{text-decoration:underline;}
 		    /*]]>*/
 		</style>
 	    </head>
 	    <body>
 	    <div style="border:1px dotted #999;margin:10px 0;padding:5px;">
-	    <h1 style="paddin:0;margin:0 0 10px 0;background:#669933;color:#fff;">TALE Error</h1>
-	    <h3 style="text-decoration:underline;">Exception Details</h3>
+	    <h1>TALE Error</h1>
+	    <h3>Exception Details</h3>
 	    <ul>
 	    <li>
 	    <strong>Calling Node:</strong>
@@ -111,7 +113,7 @@ TAL.Error = function(error, data, expression) {
 	    <code id="error"></code>
 	    </li>
 	    </ul>
-	    <h3 style="text-decoration:underline;">Additional Information</h3>
+	    <h3>Additional Information</h3>
 	    <ul>
 	    <li name="scope">
 	    <strong>Rendering Context:</strong>
@@ -154,10 +156,11 @@ TAL.Error = function(error, data, expression) {
 	tal_error..ul[1].li[3] += session_data;
 
 	var code = tal_error..code.(@id=='error')[0];
-	if (error instanceof TAL.Error)
+	if (error instanceof TAL.Error) {
 	    code += error.toXMLString();
-	else
+	} else {
 	    code.appendChild(<>{error}</>);
+	}
 
 	return tal_error;
     };
