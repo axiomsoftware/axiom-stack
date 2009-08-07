8add7c7c4a36c521887a2ddbc0b790b4077e83a1
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2009-08-07 19:46:49 -0400

    pass correct index type in test script

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-java -cp launcher.jar axiom.main.launcher.Commandline -d . -f server.properties test.test
+java -Dorg.apache.lucene.FSDirectory.class=org.apache.lucene.store.TransFSDirectory -cp launcher.jar axiom.main.launcher.Commandline -d . -f server.properties -a test run_tests
